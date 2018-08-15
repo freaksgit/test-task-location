7fc4c35f425f8bdff85eb8e02c7b27d813fb354d
Vasyl Stoliarchuk <stoliarchuk.v.vasyl@gmail.com>
2018-08-15 21:38:31 +0300

    Google Map is implemented

---
+++ app/src/main/java/vasyl/v/stoliarchuk/addresstracker/MapActivity.java
@@ -0,0 +1,27 @@
+package vasyl.v.stoliarchuk.addresstracker;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.activity_map_fragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+    }
+}
